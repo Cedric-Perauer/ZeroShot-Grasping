6de02eb76747b6d25d4ad124cbb919f896dac562
Cedric-Perauer <Cedric.perauer@icloud.com>
2023-06-13 12:23:55 +0200

    eval shell scripts

---
+++ zero-shot-pose/zsp/eval_2stage.sh
@@ -0,0 +1,3 @@
+python method/evaluate_ref_to_target_pose_2stage.py     --co3d_root /home/cedric/ZeroShot-Grasping/zero-shot-pose/co3d/     --hub_dir /home/cedric/ZeroShot-Grasping/zero-shot-pose/     --kmeans --plot_results
+
+
