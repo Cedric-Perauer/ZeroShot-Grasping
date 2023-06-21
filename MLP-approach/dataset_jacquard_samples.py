55d1aa16e0cdd56626475fb89a41437dab9f8a0e
zoenguer <baris.zoenguer@tum.de>
2023-06-21 14:49:44 +0200

    new dataset implementation for batched training

---
+++ MLP-approach/dataset_jacquard_samples.py
@@ -0,0 +1,80 @@
+from torch.utils.data import Dataset
+import torch
+import numpy as np
+from torchvision import transforms
+
+import os
+from PIL import Image
+import random
+from utils import get_grasp, get_augmented_angles
+
+jacquard_root  =os.getcwd()+ r'/Jacquard_fence/Samples/'
+
+
+class JacquardSamples(Dataset):
+    '''
+    very simple dataloader to test some stuff on Jacquard samples dataset
+    '''
+
+    def __init__(self, dataset_root=jacquard_root,
+                 image_transform=None,
+                 num_targets=1, crop=False, overfit=False, img_size=224):
+        self.img_size = img_size
+        self.dataset_root = dataset_root
+        self.image_transform = image_transform
+        self.classes = os.listdir(dataset_root)
+        self.image_norm_mean = (0.485, 0.456, 0.406)
+        self.image_norm_std = (0.229, 0.224, 0.225)
+        self.crop = crop
+        self.overfit = overfit
+        for num_c, cat in enumerate(self.classes):
+            if os.path.isdir(self.dataset_root + cat) == False:
+                continue
+            fs = os.listdir(self.dataset_root + cat)
+
+            imgs = [self.dataset_root + cat + "/" + i for i in fs if i.endswith('.jpg') or i.endswith('.png')]
+            grasp_txts = [self.dataset_root + cat + "/" + i for i in fs if i.endswith('.txt')]
+            grasp_txts = [i.split('/')[-1] for i in grasp_txts]
+            imgs = [i.split('/')[-1] for i in imgs if 'RGB' in i]
+
+            imgs = sorted(imgs, key=lambda x: int(x.split('_')[0]))
+            grasp_txts = sorted(grasp_txts, key=lambda x: int(x.split('_')[0]))
+            imgs = [self.dataset_root + cat + "/" + i for i in imgs]
+            grasp_txts = [self.dataset_root + cat + "/" + i for i in grasp_txts]
+        self.image_paths = imgs
+        self.grasp_txts = grasp_txts
+
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        data_dict = {}
+        img_raw = Image.open(self.image_paths[index])
+        img = self.image_transform(img_raw)
+
+        points_grasps, gknet_labels = get_grasp(self.grasp_txts[index], self.img_size, self.crop)
+        points_grasps = torch.tensor(points_grasps).squeeze()
+        gknet_labels = torch.tensor(gknet_labels).squeeze()
+        data_dict['points_grasp'] = torch.tensor(points_grasps)
+        data_dict['img'] = img
+        data_dict['angle'] = []
+        data_dict['img_grasp'] = []
+        data_dict['points_grasp_augmented'] = []
+        data_dict['points_grasp_augmented2'] = []
+        data_dict['img_augmented_grasp'] = []
+        data_dict['img_augmented_grasp2'] = []
+        for i in range(gknet_labels.shape[0]):
+            angle = random.randint(-180, 180)
+            data_dict["angle"].append(angle)
+            augmented_gknet_label, augmented_gknet_label2, gknet_label, pg_aug, pg_aug2 = get_augmented_angles(gknet_labels[i].numpy().copy(), angle, self.img_size)
+            data_dict['img_grasp'].append(gknet_label)
+            data_dict['points_grasp_augmented'].append(pg_aug)
+            data_dict['points_grasp_augmented2'].append(pg_aug2)
+            data_dict['img_augmented_grasp'].append(augmented_gknet_label)
+            data_dict['img_augmented_grasp2'].append(augmented_gknet_label2)
+        return data_dict
+
+
+
+
---
+++ MLP-approach/utils.py
@@ -0,0 +1,129 @@
+import torch
+import numpy as np
+import torchvision.transforms.functional as TF
+import math
+
+
+def distance(ax, ay, bx, by):
+    return math.sqrt((by - ay) ** 2 + (bx - ax) ** 2)
+
+def rotated_about(ax, ay, bx, by, angle):
+    radius = distance(ax, ay, bx, by)
+    angle += math.atan2(ay - by, ax - bx)
+
+    # import pdb; pdb.set_trace()
+    try:
+        return [
+            round(bx.item() + radius * math.cos(angle)),
+            round(by.item() + radius * math.sin(angle))
+        ]
+    except:
+        return [
+            round(bx + radius * math.cos(angle)),
+            round(by + radius * math.sin(angle))
+        ]
+
+def create_grasp_rectangle(grasp):
+    mids = []
+    grasps = []
+    centers = []
+    gripper_poses = []
+    for n, el in enumerate(grasp):
+        # import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
+        x, y, angle, w, h = el
+        # if [x,y] in mids: deactivate for the plots
+        #    continue
+        mids.append([x, y])
+        tl = (x - w / 2, y - h / 2)
+        bl = (x - w / 2, y + h / 2)
+        tr = (x + w / 2, y - h / 2)
+        br = (x + w / 2, y + h / 2)
+        # use left and right point representation as in GKNet, choose the midpoints of the vertical lines
+        mr = ((br[0] + tr[0]) * 0.5, (br[1] + tr[1]) * 0.5)
+        ml = ((bl[0] + tl[0]) * 0.5, (bl[1] + tl[1]) * 0.5)
+
+        points = [br, tr, tl, bl]
+        grippers = [ml, mr]
+        # tl,bl,tr,br = self.rotate_points((x,y),points,angle)
+        # print(len(points))
+        square_vertices = [rotated_about(pt[0], pt[1], x, y, math.radians(angle)) for pt in points]
+        gripper_points = [rotated_about(pt[0], pt[1], x, y, math.radians(angle)) for pt in grippers]
+        grasps.append(square_vertices)
+        centers.append([x, y, angle, w, h])
+        gripper_poses.append(gripper_points)
+        # br,tr,tl,bl = square_vertices
+    # import pdb; pdb.set_trace()
+    return grasps, centers, gripper_poses
+
+
+def get_grasp(grasp_txt, img_size, crop):
+    out_grasps = []
+    grasping_labels = []
+    raw_grasp_labels = []
+    gripper_labels = []
+    grasp = []
+    with open(grasp_txt, 'r') as f:
+        lines = f.readlines()
+        for l in lines:
+            split = l.split(';')
+            x, y, angle, w, h = split
+            h = h.split('\n')[0]
+            x, y, angle, w, h = float(x), float(y), float(angle), float(w), float(h)
+            if crop == False:
+                grasp.append([x * img_size / 1024., y * img_size / 1024., angle, w * img_size / 1024,
+                               h * img_size / 1024])
+            else:
+                grasp.append([x, y, angle, w, h])
+
+        raw_grasp_labels.append(grasp)
+        if crop == False:
+            grasps, raw_labels, gripper_points = create_grasp_rectangle(grasp)
+            grasping_labels.append(grasps)
+            gripper_labels.append(gripper_points)
+            out_grasps.append(grasp)
+        return np.array(gripper_labels), np.array(out_grasps)
+
+def augment_image(image, angle=10):
+    ##rotate the image
+    image_augmented = TF.rotate(image, angle)
+    return image_augmented
+
+def get_augmented_angles(augmented_gknet_label, angle, img_size):
+    gknet_label = augmented_gknet_label.copy()[:4]
+    augmented_angle = augmented_gknet_label[2] - angle  # just change the angle of the label here
+    augmented_angle2 = augmented_angle
+    if augmented_angle < 0:
+        augmented_angle2 = augmented_angle + 180
+    else:
+        augmented_angle2 = augmented_angle - 180
+
+    xr, yr = rotated_about(augmented_gknet_label[0], augmented_gknet_label[1], img_size / 2.,
+                                img_size / 2., math.radians(-angle))
+    x = xr / img_size
+    y = yr / img_size
+    angle_cos = math.cos(math.radians(augmented_angle))
+    angle_sin = math.sin(math.radians(augmented_angle))
+
+    angle_cos2 = math.cos(math.radians(augmented_angle2))
+    angle_sin2 = math.sin(math.radians(augmented_angle2))
+    wr = augmented_gknet_label[3]
+    w = augmented_gknet_label[3] / img_size
+    augmented_gknet_label = torch.tensor([x, y, angle_cos, angle_sin, w])
+    augmented_gknet_label2 = torch.tensor([x, y, angle_cos2, angle_sin2, w])
+
+    xt, yt, angle_label, wt = gknet_label
+    gknet_label = [xt / img_size, yt / img_size, math.cos(math.radians(angle_label)),
+                   math.sin(math.radians(angle_label)), wt / img_size]
+    '''
+    if self.crop == False :
+        grasps_ref = grasps_ref * self.img_size/1024
+    '''
+    lxt, lyt = xr - wr / 2., yr
+    rxt, ryt = xr + wr / 2., yr
+    rxt, ryt = rotated_about(rxt, ryt, float(xr), float(yr), math.radians(augmented_angle))
+    lxt, lyt = rotated_about(lxt, lyt, float(xr), float(yr), math.radians(augmented_angle))
+    return augmented_gknet_label, augmented_gknet_label2, gknet_label,\
+           torch.tensor([[lxt, lyt], [rxt, ryt]]), torch.tensor([[rxt, ryt], [lxt, lyt]])
+
+
