51553e77e4fa7076e25d6b84414d4e631d3aab03
Cedric-Perauer <Cedric.perauer@icloud.com>
2023-07-13 16:41:37 +0200

    support topk inference and fix metrics bugs in infer_grasp

--- MLP-approach/infer_utils.py
+++ MLP-approach/infer_utils.py
@@ -51,6 +51,40 @@ def test_single_point(grasp, mask, device, features, model, args_infer, point_id
         preds = model(mean_feats.to(device), dif_n.to(device))
     return preds.squeeze(), dif_n.squeeze(), single_point, single_point_gt, height
 
+def test_two_points(grasp, mask, device, features, model, args_infer, point_idx,single_point=None):
+    if single_point is not None : 
+        pass
+    else : 
+        single_point = grasp[point_idx]
+    
+    single_point = single_point.unsqueeze(0).repeat(args_infer["img_size"] // 14 * args_infer["img_size"] // 14,
+                                                    1).unsqueeze(1)
+    all_points = torch.tensor(
+        [[i, j] for j in range(args_infer["img_size"] // 14) for i in range(args_infer["img_size"] // 14)]).unsqueeze(1)
+    
+    all_points = torch.cat([single_point, all_points], dim=1).to(device)
+    mean_feats = []
+    dif = (all_points[:, 0, :] - all_points[:, 1, :]).type(torch.float32).norm(p=2, dim=1)
+    dif_n = (dif / mask).unsqueeze(1)
+    for i in range(all_points.shape[0]):
+        imix = int(all_points[i, :, 0].min().cpu())
+        imax = int(all_points[i, :, 0].max().cpu())
+        ymix = int(all_points[i, :, 1].min().cpu())
+        ymax = int(all_points[i, :, 1].max().cpu())
+        try : 
+            features_i = features[imix:imax + 1, ymix:ymax + 1, :]
+        except : 
+            import pdb; pdb.set_trace()
+        # attn_i = attn_norms[imix:imax+1, ymix:ymax+1].mean()
+        features_i = features_i.reshape(features_i.shape[0] * features_i.shape[1], features_i.shape[2]).mean(0)
+        if i == 0:
+            mean_feats = features_i.unsqueeze(0)
+        else:
+            mean_feats = torch.cat([mean_feats, features_i.unsqueeze(0)], dim=0)
+    with torch.no_grad():
+        preds = model(mean_feats.to(device), dif_n.to(device))
+    return preds.squeeze(), dif_n.squeeze(), single_point, None
+
 def test_single_point_constraint(grasp, mask, device, features, model, args_infer, point_idx,constrain_mode=True):
     single_point = grasp[point_idx].reshape(2,)
     
