ba26abba4cb83018f1237af07912d6091f754394
Cedric-Perauer <Cedric.perauer@icloud.com>
2023-07-19 17:42:25 +0200

    category creator change

--- MLP-approach/create_category_sets.py
+++ MLP-approach/create_category_sets.py
@@ -29,12 +29,14 @@ for line in lines:
         # Add the object ID to the current name's list
         data_dict[current_name].append(line)
 
-if os.path.exists('Dataset/categories') == True:
-        shutil.rmtree('Dataset/categories')
 
-os.makedirs('new_data')
 base = 'Dataset/categories/'
+if os.path.exists(base) == True:
+        shutil.rmtree(base)
+os.makedirs(base)
+
 full_data_pth = 'Dataset/Full_Dataset/'
+AJD_path = os.path.expanduser('~') + '/GraspKpNet/datasets/Jacquard/coco/512_cnt_angle/train/grasps_train2018/'
 
 splits = ['train', 'test']
 for obj in data_dict:
@@ -61,7 +63,20 @@ for obj in data_dict:
                                 for relevant_object in relevant_objects: 
                                         if relevant_object in cur_full_name:
                                                 new_dir_name = cur_base_dir + relevant_object + '/'
+                                                ajd_pth = AJD_path + '/'.join(cur_full_name.split('/')[2:]) 
+                                                fs = os.listdir(ajd_pth)
+                                                existing_nums = []
+                                                for f in fs : 
+                                                        if f.endswith('.png'):
+                                                                existing_nums.append(int(f.split('_')[0]))      
+                                                #import pdb; pdb.set_trace()     
                                                 shutil.copytree(cur_full_name, new_dir_name)
+                                                fs = os.listdir(new_dir_name)
+                                                for f in fs : 
+                                                        start_num = int(f.split('_')[0])        
+                                                        if start_num not in existing_nums:
+                                                                os.remove(new_dir_name + f)
+                                                
                                         
 								
                                 
