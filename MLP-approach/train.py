b3fc4f0f4cdb7af343a311bef9fb0326746c5645
Cedric-Perauer <cedric.perauer@tum.de>
2023-09-09 22:53:57 +0200

    first running unet version training

--- MLP-approach/train.py
+++ MLP-approach/train.py
@@ -1,17 +1,18 @@
-from train_jac_single import main
+#from train_jac_single import main
+from train_jac_unet import main
 
 
 args_train = {
-    "split" : "Figure_train/",
+    "split" : "Bottle_train/",
     "device": "cuda",
     "angle_mode": False,
     "img_size": 1120,
     "num_epochs": 100,
     "num_objects": 1,
-    "lr": 5e-4,
+    "lr": 1e-3,
     "batch_size": 64,
     "print_every_n": 1,
-    "experiment_name": "figure_1_double_sampling"
+    "experiment_name": "bottle_1_double_sampling"
 }
 
 main(args_train)
